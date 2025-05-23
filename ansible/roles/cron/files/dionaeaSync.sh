1ddf14750eed058493d113389688a44d0d828891
Alejandra Beltrán <beltranmorales.ale@gmail.com>
2025-05-23 13:20:48 +0200

    Update dionaeaSync.sh

--- ansible/roles/cron/files/dionaeaSync.sh
+++ ansible/roles/cron/files/dionaeaSync.sh
@@ -2,7 +2,7 @@
 
 SRC_DIR="/opt/dionaea/var/lib/dionaea/"
 SRC_DIR2="/opt/cowrie/"
-DEST_DIR="alebm@192.168.128....:~/dionaea/"
+DEST_DIR="alebm@192.168.128.xxx:~/dionaea/"
 
 rsync -avz --progress \
     "${SRC_DIR}/binaries" \
