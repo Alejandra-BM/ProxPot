7f6e30a60de6887e70ef20abfd2ba013760e0a5c
Alejandra Beltrán <beltranmorales.ale@gmail.com>
2025-05-23 13:18:03 +0200

    Update dionaeaSync.sh

--- ansible/roles/cron/files/dionaeaSync.sh
+++ ansible/roles/cron/files/dionaeaSync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SRC_DIR="/opt/dionaea/var/lib/dionaea/"
-DEST_DIR="alebm@192.168.128...:~/dionaea/"
+SRC_DIR2="/opt/cowrie/"
+DEST_DIR="alebm@192.168.128....:~/dionaea/"
 
 rsync -avz --progress \
     "${SRC_DIR}/binaries" \
     "${SRC_DIR}/bistreams" \
     "${SRC_DIR}/ftp" \
     "${SRC_DIR}/http" \
+    "${SRC_DIR2}/logs" \
     "${DEST_DIR}"
